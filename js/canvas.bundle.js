ccd6d04301e6415491e35df190b8c4fdea937038
Aleshere <55036177+Aleshere@users.noreply.github.com>
2020-06-28 10:38:05 +0100

    Add files via upload

---
+++ js/canvas.bundle.js
@@ -0,0 +1,196 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/canvas.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/canvas.js":
+/*!**************************!*\
+  !*** ./src/js/canvas.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+var mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+}; // const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
+
+var colors = ['#FFA41B', '#000839', '#005082', '#00A8CC']; // Event Listeners
+
+addEventListener('mousemove', function (event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+addEventListener('resize', function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init();
+}); // Objects
+
+function Particle(x, y, radius, color) {
+  var _this = this;
+
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
+  this.radians = Math.random() * Math.PI * 2;
+  this.velocity = 0.05;
+  this.distanceFromCenter = randomIntFromRange(50, 120);
+  this.lastMouse = {
+    x: x,
+    y: y
+  };
+
+  this.update = function () {
+    var lastPoint = {
+      x: _this.x,
+      y: _this.y
+    }; // Move circles over time
+
+    _this.radians += _this.velocity; // Drag Effect
+
+    _this.lastMouse.x += (mouse.x - _this.lastMouse.x) * 0.05;
+    _this.lastMouse.y += (mouse.y - _this.lastMouse.y) * 0.05; // Circular Motion
+
+    _this.x = _this.lastMouse.x + Math.cos(_this.radians) * _this.distanceFromCenter;
+    _this.y = _this.lastMouse.y + Math.sin(_this.radians) * _this.distanceFromCenter;
+
+    _this.draw(lastPoint);
+  };
+
+  this.draw = function (lastPoint) {
+    c.beginPath();
+    c.strokeStyle = _this.color;
+    c.lineWidth = _this.radius;
+    c.moveTo(lastPoint.x, lastPoint.y);
+    c.lineTo(_this.x, _this.y);
+    c.stroke();
+    c.closePath();
+  };
+} // Implementation
+
+
+var particles;
+
+function init() {
+  particles = [];
+
+  for (var i = 0; i < 70; i++) {
+    var radius = Math.random() * 2 + 1;
+    particles.push(new Particle(canvas.width / 2, canvas.height / 2, radius, randomColor(colors)));
+  }
+} // Animation Loop
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = 'rgba(255,255,255,.05)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(function (particle) {
+    particle.update();
+  });
+}
+
+init();
+animate();
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=canvas.bundle.js.map
